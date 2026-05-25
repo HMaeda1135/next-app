3c7a098ae3788354639deff4478f3d2d129df04e
DSS Engineer <engineer11355311921@gmail.com>
2026-05-25 22:14:08 +0900

    fix: metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About",
+    description: "About page",
+};
+
+
 export default function AboutPage() {
     return (
         <div className="card">
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,13 +1,6 @@
-import type { Metadata } from "next";
-
 type Post = {
     id: number;
     title: string;
-};
-
-export const metadata: Metadata = {
-    title: "About",
-    description: "About page",
 };
 
 async function sleep(ms: number): Promise<void> {
